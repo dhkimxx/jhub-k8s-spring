1c307e2629ad0013d314082d03c3f6c7acc865a5
dhkimxx <dhkimxx@naver.com>
2026-01-12 15:51:56 +0900

    init

---
+++ src/main/java/com/dhkimxx/jhub_k8s_spring/JhubK8sSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dhkimxx.jhub_k8s_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhubK8sSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhubK8sSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhkimxx/jhub_k8s_spring/JhubK8sSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhkimxx.jhub_k8s_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhubK8sSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
